0623454f8ca79f95534b43fd9dbe5ab3984a2151
Rifki Salim <contact@rifkisalim.com>
2023-10-09 09:33:13 +0530

    🌈 style(QuoteBody): add glassmorphic UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FaQuoteLeft, FaTwitter } from "react-icons/fa";
 import { FiRefreshCw } from "react-icons/fi";
+import BlobsBG from "./assets/bg-blobs.svg";
 
 function App() {
   return (
@@ -8,31 +9,39 @@ function App() {
       className="container-fluid"
       style={{
         height: "100vh",
-        background:
-          "radial-gradient(circle at top left,transparent 9%, #4f46e4 10% ,#4f46e4 15% , transparent 16%) , radial-gradient(circle at bottom left,transparent 9%, #4f46e4 10% ,#4f46e4 15% , transparent 16%), radial-gradient(circle at top right ,transparent 9%, #4f46e4 10% ,#4f46e4 15% , transparent 16%) , radial-gradient(circle at bottom right,transparent 9%, #4f46e4 10% ,#4f46e4 15% , transparent 16%),radial-gradient(circle, transparent 25%, #ffffff  26%),linear-gradient(0deg, transparent 44%, #4f46e4 45%, #4f46e4 55%, transparent 56%), linear-gradient(90deg, transparent 44%, #4f46e4 45%, #4f46e4 55%, transparent 56%)",
-        backgroundSize: "6em 6em",
-        backgroundColor: "#ffffff",
-        opacity: 1,
+        backgroundImage: `url(${BlobsBG}) `,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundPosition: "center",
       }}
     >
       <div className="container d-flex w-100 h-100 align-items-center justify-content-center p-0">
         <div className="col-lg-10 col-12 p-2">
-          <div className="card" id="quote-box">
+          <div
+            className="card rounded-3 shadow"
+            id="quote-box"
+            style={{
+              background: "rgba(218, 218, 218, 0.20)",
+              backdropFilter: "blur(7.5px)",
+              WebkitBackdropFilter: "blur(7.5px)",
+            }}
+          >
             <div className="card-body p-4 d-flex flex-column justify-content-center align-items-center">
               <FaQuoteLeft size={56} />
               <figure className="text-center">
                 <blockquote className="blockquote" id="text">
-                  <h4 className="display-4">Deez nuts</h4>
+                  <h4 className="display-4 text-white">This is a quote</h4>
                 </blockquote>
                 <figcaption
-                  className="blockquote-footer fw-light fs-5"
+                  className="blockquote-footer fw-light text-white text-opacity-75 fs-4"
                   id="author"
                 >
                   Someone famous
                 </figcaption>
               </figure>
               <button
-                className="btn btn-success d-flex align-items-center"
+                className="btn btn-primary fw-bold d-flex align-items-center"
                 id="new-quote"
               >
                 <FiRefreshCw className="me-2" />
@@ -42,7 +51,7 @@ function App() {
             <div className="card-footer d-flex p-2 justify-content-center align-items-center">
               <a
                 href="https://twitter.com/intent/tweet"
-                className="btn btn-primary d-flex align-items-center "
+                className="btn btn-outline-secondary text-white d-flex align-items-center "
                 id="tweet-quote"
               >
                 <FaTwitter size={28} className="me-2" />
