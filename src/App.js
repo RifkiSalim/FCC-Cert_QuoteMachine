03c2d8a8e6e4be6ea4172b041f9dbaaefd3e3c2b
Rifki Salim <contact@rifkisalim.com>
2023-10-09 11:29:08 +0530

    ✨ feat(QuoteCard): implement req from api

--- src/App.js
+++ src/App.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FaQuoteLeft, FaTwitter } from "react-icons/fa";
 import { FiRefreshCw } from "react-icons/fi";
 import BlobsBG from "./assets/bg-blobs.svg";
-
+import axios from "axios";
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [quote, setQuote] = useState({
+    body: "",
+    author: "",
+  });
+  const getQuote = () => {
+    setIsLoading(true);
+    axios
+      .get("https://api.quotable.io/quotes/random")
+      .then(function (response) {
+        const { data } = response;
+        setQuote({
+          body: data[0].content,
+          author: data[0].author,
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .finally(function () {
+        setIsLoading(false);
+      });
+  };
+  useEffect(() => {
+    getQuote();
+  }, []);
   return (
     <div
       className="container-fluid"
       style={{
-        height: "100vh",
+        height: "100dvh",
         backgroundImage: `url(${BlobsBG}) `,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
@@ -29,20 +55,28 @@ function App() {
           >
             <div className="card-body p-4 d-flex flex-column justify-content-center align-items-center">
               <FaQuoteLeft size={56} />
-              <figure className="text-center">
+              <figure
+                className="text-center"
+                style={{
+                  opacity: isLoading ? 0 : 1, // Apply opacity based on isLoading state
+                  transition: "opacity 0.75s", // Add a transition effect
+                }}
+              >
                 <blockquote className="blockquote" id="text">
-                  <h4 className="display-4 text-white">This is a quote</h4>
+                  <h4 className="display-4 text-white">{quote.body}</h4>
                 </blockquote>
                 <figcaption
                   className="blockquote-footer fw-light text-white text-opacity-75 fs-4"
                   id="author"
                 >
-                  Someone famous
+                  {quote.author}
                 </figcaption>
               </figure>
               <button
                 className="btn btn-primary fw-bold d-flex align-items-center"
                 id="new-quote"
+                disabled={isLoading}
+                onClick={() => getQuote()}
               >
                 <FiRefreshCw className="me-2" />
                 New Quote
@@ -50,7 +84,11 @@ function App() {
             </div>
             <div className="card-footer d-flex p-2 justify-content-center align-items-center">
               <a
-                href="https://twitter.com/intent/tweet"
+                rel="noreferrer"
+                target="_blank"
+                href={`https://twitter.com/intent/tweet?hashtags=quotes&text=${encodeURIComponent(
+                  `"${quote.body}" \n- ${quote.author}`
+                )}`}
                 className="btn btn-outline-secondary text-white d-flex align-items-center "
                 id="tweet-quote"
               >
